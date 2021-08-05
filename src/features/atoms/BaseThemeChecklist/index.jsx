fcc9ccafc5a8683076f4b2efca8d831f20663812
pqthinh <phamquangquang2008@gmail.com>
2021-08-05 23:59:26 +0700

    active module"

--- src/features/atoms/BaseThemeChecklist/index.jsx
+++ src/features/atoms/BaseThemeChecklist/index.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import { Wrapper, Image, Title } from './styled'
 import PropTypes from 'prop-types'
 
-const BaseThemeChecklist = ({ imageUrl, content, ...others }) => {
+const BaseThemeChecklist = ({ imageUrl, content, active, ...others }) => {
   return (
-    <Wrapper {...others}>
+    <Wrapper check={active} {...others}>
       <Image source={imageUrl} />
-      <Title>{content || ''}</Title>
+      <Title>{content}</Title>
     </Wrapper>
   )
 }
 
 BaseThemeChecklist.propTypes = {
   imageUrl: PropTypes.string,
-  content: PropTypes.string
+  content: PropTypes.string,
+  active: PropTypes.bool
 }
 
 export default React.memo(BaseThemeChecklist)
