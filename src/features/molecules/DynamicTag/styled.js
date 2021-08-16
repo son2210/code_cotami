32df622a0e0226ae4985aec3f1dbe06f9bd5f048
linh ho mau tuan <linhfl@its-global.vn>
2021-08-16 10:29:49 +0000

    Create Account UI

--- src/features/molecules/DynamicTag/styled.js
+++ src/features/molecules/DynamicTag/styled.js
@@ -4,9 +4,11 @@ import styled from 'styled-components'
 export const Wrapper = styled(FormGroup)``
 
 export const Label = styled(ControlLabel)`
+  font-size: 1rem;
   font-weight: 500 !important;
   margin-bottom: 10px !important;
   display: flex !important;
+  color: ${({ theme }) => theme.colors.secondary[1]};
 `
 
 export const TagItem = styled(Tag)`
--- src/utils/Routers/index.js
+++ src/utils/Routers/index.js
@@ -32,13 +32,7 @@ const Routers = {
       },
       {
         URL: '/account-management',
-        NAME: 'account-management',
-        CHILD: [
-          {
-            URL: '/accounts-management/create-account',
-            NAME: 'Create an account'
-          }
-        ]
+        NAME: 'account-management'
       }
     ],
 
@@ -68,6 +62,17 @@ const Routers = {
         {
           URL: '/profile/update-profile',
           NAME: 'Update password'
+        }
+      ]
+    },
+
+    ACCOUNT: {
+      URL: '/account-management',
+      NAME: 'Checklist management',
+      CHILD: [
+        {
+          URL: '/account-management/create',
+          NAME: 'Create a account'
         }
       ]
     }
